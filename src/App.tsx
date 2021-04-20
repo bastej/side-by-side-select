d7a7c6796b09768c8f9a50ef7fb37f2601cdb73f
bastej <siatkasebastian@gmail.com>
2021-04-20 20:29:14 +0200

    Changes to adjust view

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SideBySideSelect from "./components/SideBySideSelect/SideBySideSelect";
+
+import styles from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.Select}>
+      <SideBySideSelect
+        allOptions={["a", "b", "c", "d", "e"]}
+        label="Test-label"
+        texts={{
+          allItemsHeader: "All",
+          selectedItemsHeader: "Selected",
+        }}
+        initialSelectedOptions={["a", "b", "e"]}
+        onChange={() => {
+          console.log("Do extra action");
+        }}
+      />
     </div>
   );
 }
